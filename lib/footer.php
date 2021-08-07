28459fbb36f9a5ae2aa6c42183ef6d986e1651f3
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-08-07 21:20:15 +0200

    Remove dumb w3 validator thing in footer.

--- lib/footer.php
+++ lib/footer.php
@@ -1,7 +1,6 @@
 	<table class="ptable" id="pagefooter">
 		<tr>
 			<td class="c1 left vmiddle">
-				<span style="float: left; margin-right: 5px;"><a href="http://validator.w3.org/check?uri=referer" target="_blank"><img src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Transitional" height="31" width="88" title="this is HTML5 but who cares"></a></span>
 				<span style="float: right; text-align: right;">
 					<?=sprintf("Page rendered in %1.3f seconds. (%dKB of memory used)", $rendertime, memory_get_usage(false) / 1024) ?>
 				</span>
