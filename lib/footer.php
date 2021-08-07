dd46dc902c63cbd4abe5bf0c94a367b997e5248d
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-08-07 21:18:58 +0200

    Remove mySQL junk from footer.

--- lib/footer.php
+++ lib/footer.php
@@ -2,7 +2,9 @@
 		<tr>
 			<td class="c1 left vmiddle">
 				<span style="float: left; margin-right: 5px;"><a href="http://validator.w3.org/check?uri=referer" target="_blank"><img src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Transitional" height="31" width="88" title="this is HTML5 but who cares"></a></span>
-				<span style="float: right; text-align: right;">Page rendered in <?php printf('%01.3f', $rendertime); ?> seconds.<br>MySQL: <?php print $nqueries; ?> queries, <?php print $nrowsf.'/'.$nrowst; ?> rows.</span>
+				<span style="float: right; text-align: right;">
+					<?=sprintf("Page rendered in %1.3f seconds. (%dKB of memory used)", $rendertime, memory_get_usage(false) / 1024) ?>
+				</span>
 				Powered by Kuriblog v1.1
 			</td>
 		</tr>
